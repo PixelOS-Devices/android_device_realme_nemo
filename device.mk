d62c66e77719bdee3407216b166d6dc66f53618f
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    nemo: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- device.mk
+++ device.mk
@@ -157,6 +157,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    NexusLauncherOverlay \
     SettingsOverlayNemo \
     SettingsProviderOverlay6 \
     SettingsProviderOverlay6i \
